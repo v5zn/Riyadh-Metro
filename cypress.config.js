a7754f5d6cda133551233b26acd9903515c44986
Mohamed Yousif Idris <mohammedyousifstan@gmail.com>
2025-05-11 18:04:00 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080", // or whatever local address you test on
-    specPattern: "cypress/e2e/**/*.cy.js"
+    baseUrl: "http://localhost:8080",
+    specPattern: "cypress/e2e/**/*.cy.js",
+    supportFile: false
   }
 });
