f111c9a6ad544d187eef07417845b9398a03ed30
Mohamed Yousif Idris <mohammedyousifstan@gmail.com>
2025-05-15 01:53:57 +0300

    Delete cypress/e2e/homepage.cy.js

--- cypress/e2e/homepage.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Riyadh Metro Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('/')
-    cy.contains('Riyadh Metro') // replace with actual text on your homepage
-  })
-})
